15c7cb334c44798b65c90131404f109b762d8f63
wahyudidarmini <36563602+wahyudidarmini@users.noreply.github.com>
2018-02-18 02:58:37 +0700

    Update yudi5Bot.py

--- yudi5Bot.py
+++ yudi5Bot.py
@@ -6,23 +6,23 @@ from datetime import datetime
 import time,random,sys,json,codecs,threading,glob,re,os,subprocess
 
 cl = LINETCR.LINE() #Luffy
-cl.login(qr=True)
+cl.login(token="Epgwk9q1BCl1SAeAWqS2.AZs3a8Vf+ipnhLTOmQbtuG.XhBLQ2Q9LsgxKnthI5KtL9N8Ca4Gthhu0YNqBYdomio=")
 cl.loginResult()
 
 ki = LINETCR.LINE() #Zorro
-ki.login(qr=True)
+ki.login(token="Epgwk9q1BCl1SAeAWqS2.AZs3a8Vf+ipnhLTOmQbtuG.XhBLQ2Q9LsgxKnthI5KtL9N8Ca4Gthhu0YNqBYdomio=")
 ki.loginResult()
 
 kk = LINETCR.LINE() #Sanji
-kk.login(qr=True)
+kk.login(token="EpMurECRUQIprLv8sbfd.r6+IKdkmt6TsWXW3sBT0Bq.IyOgaAheiKmOEQTPIlVwnxgVPhFfmOL6PYxUNNR/AcM=")
 kk.loginResult()
 
 kc = LINETCR.LINE() #Ussop
-kc.login(qr=True)
+kc.login(token="EpAIvEH4pMxP8mc83Ip9.xK5vGW/aYahp8aZbgO7kwq.8c6gvkk8AnDoqil24nFr3tNMFpc413iB4F3zjpQwaD0=")
 kc.loginResult()
 
 ks = LINETCR.LINE() #Chooper
-ks.login(qr=True)
+ks.login(token="EpzdzdaR3HwUoKPPX6Ab.G/uF5Lybw5sPkxIi2moeEW.bHNiASiytKsJ+665c4Y6aQt4kzBatDzyIhcm5DoNV3M=")
 ks.loginResult()
 
 print "login success plak"
@@ -76,8 +76,8 @@ Cmid = kc.getProfile().mid #Ussop
 Dmid = ks.getProfile().mid #Chooper
 
 Bots=[mid,Amid,Bmid,Cmid,Dmid]
-admin=["ued156c86ffa56024c0acba16f7889e6d"] 
-owner=["ued156c86ffa56024c0acba16f7889e6d"]
+admin=["u6b34b703cbc5fc83cd1e5b6832a05352"] 
+owner=["u6b34b703cbc5fc83cd1e5b6832a05352"]
 whitelist=[""]
 wait = {
     'contact':False,
